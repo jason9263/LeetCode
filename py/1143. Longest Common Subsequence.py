2979d1411222affb69af722309829edc66a65b74
JDI9263 <jason9263@163.com>
2022-03-28 09:47:14 -0700

    daily update

---
+++ py/1143. Longest Common Subsequence.py
@@ -0,0 +1,17 @@
+
+class Solution:
+
+    def longestCommonSubsequence(self, a: str, b: str) -> int:
+        M, N = len(a), len(b)
+        t = [[0 for _ in range(N+1)] for _ in range(M+1)]
+        for m in range(1, M+1):
+            for n in range(1, N+1):
+                if a[m-1] == b[n-1]:
+                    t[m][n] = 1 + t[m-1][n-1]
+                else:
+                    t[m][n] = max(t[m][n-1], t[m-1][n])
+
+        return t[M][N]
+        # print(t, type(t))
+        # t = [[0] * (N + 1) ] * (M + 1)
+        # print(t, type(t))
