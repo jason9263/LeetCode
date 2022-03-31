3954426a19844b23edf7f7adef77c7fa87aaa3b4
JDI9263 <jason9263@163.com>
2022-03-31 10:28:35 -0700

    daily update

--- py/311. Sparse Matrix Multiplication.py
+++ py/311. Sparse Matrix Multiplication.py
@@ -27,4 +27,42 @@ class Solution:
         
         return ans
             
+
+class Solution:
+    def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
+        ans = []
+        if not mat1 or not mat2:
+            return ans
+        m = len(mat1)
+        n = len(mat1[0])
+        k = len(mat2)
+        l = len(mat2[0])
+        
+        if n != k:
+            return ans
+        
+        def compressM(mat):
+            r = len(mat)
+            c = len(mat[0])
+            ans = [[] for _ in range(r)]
+            
+            for r, row in enumerate(mat):
+                for c, v in enumerate(row):
+                    if v:
+                        ans[r].append((v, c))
+            return ans
+                    
+        
+        ans = [[0] * l for i in range(m)]
+        
+        A = compressM(mat1)
+        B = compressM(mat2)
+        
+        for i in range(m):
+            for v1, c1 in A[i]:
+                for v2, c2 in B[c1]:
+                    ans[i][c2] += v1*v2
+        
+        return ans
+            
         
