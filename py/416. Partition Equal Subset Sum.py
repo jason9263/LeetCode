b90ef730388e589fe2a08f30627bdeb0bce0204c
JDI9263 <jason9263@163.com>
2022-04-12 23:10:01 -0700

    daily update

--- py/416. Partition Equal Subset Sum.py
+++ py/416. Partition Equal Subset Sum.py
@@ -22,3 +22,23 @@ class Solution:
                     dp[i][j] = dp[i - 1][j] or dp[i - 1][j - cur]
 
         return dp[n][target]
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+
+        total = sum(nums)
+        if total % 2 == 1:
+            return False
+        
+        target = total // 2
+        n = len(nums)
+        
+        dp = [False] * (target + 1)
+        dp[0] = True
+        
+        for cur in nums:
+            for j in range(target, cur - 1, -1):
+                dp[j] = dp[j] or dp[j - cur]
+            
+        return dp[-1]
+        
