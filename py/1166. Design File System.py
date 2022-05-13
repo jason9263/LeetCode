ca1b56a8a0f80b40f48d99f3f17c03e966a59a2d
JDI9263 <jason9263@163.com>
2022-05-12 21:26:34 -0700

    daily update

--- py/1166. Design File System.py
+++ py/1166. Design File System.py
@@ -37,8 +37,3 @@ class FileSystem:
 
         return node.value
 
-
-# Your FileSystem object will be instantiated and called as such:
-# obj = FileSystem()
-# param_1 = obj.createPath(path,value)
-# param_2 = obj.get(path)
